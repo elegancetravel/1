1aaa2971bd17fec8d2e65f7ef40586478e57554e
Ivan Apolinar <mg5elegancetravel@gmail.com>
2026-02-05 13:56:24 -0600

    Actualizacion desde VS Code

--- subir_cambios.bat
+++ subir_cambios.bat
@@ -2,6 +2,7 @@
 echo Subiendo cambios a GitHub...
 git add .
 git commit -m "Actualizacion desde VS Code"
+git pull origin main --rebase
 git push origin main
 echo.
 echo Listo! Tus cambios se han subido.
